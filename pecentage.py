b670b3e0fb8d9ec367b97531da5730a125bcdb25
shabad-hora <aashihora20@gmail.com>
2025-09-20 18:41:22 +0100

    Add files via upload

---
+++ pecentage.py
@@ -0,0 +1,10 @@
+print("enter marks obtained in 4 subjects:")
+math=int(input("maths:"))
+english=int(input("english:"))
+science=int(input("science:"))
+spainish=int(input("spainish:"))
+sum=math=english+science+spainish
+print("sum of math,engish,science,spainish")
+perc=(sum/400)*100
+print(end="percentage mark=")
+print(perc)
